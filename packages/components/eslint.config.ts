df6b9526cc2212728e02ba8d2542010b63a3677f
Alexander Mravcak <alex.mravcak@gmail.com>
2026-02-17 21:50:56 +0100

    feat: add storybook

--- packages/components/eslint.config.ts
+++ packages/components/eslint.config.ts
@@ -3,6 +3,8 @@ import { defineConfigWithVueTs, vueTsConfigs } from '@vue/eslint-config-typescri
 import pluginVue from 'eslint-plugin-vue'
 import pluginVitest from '@vitest/eslint-plugin'
 import pluginOxlint from 'eslint-plugin-oxlint'
+// For more info, see https://github.com/storybookjs/eslint-plugin-storybook#configuration-flat-config-format
+import storybook from 'eslint-plugin-storybook'
 
 // To allow more languages other than `ts` in `.vue` files, uncomment the following lines:
 // import { configureVueProject } from '@vue/eslint-config-typescript'
@@ -26,4 +28,5 @@ export default defineConfigWithVueTs(
   },
 
   ...pluginOxlint.buildFromOxlintConfigFile('.oxlintrc.json'),
+  ...storybook.configs['flat/recommended'],
 )
--- packages/components/vite.config.ts
+++ packages/components/vite.config.ts
@@ -5,11 +5,15 @@ import vue from '@vitejs/plugin-vue'
 import vueDevTools from 'vite-plugin-vue-devtools'
 import tailwindcss from '@tailwindcss/vite'
 
+// Storybook conflicts with vue-devtools
+// Workaround: https://github.com/storybookjs/storybook/issues/32462#issuecomment-3401454726
+const isStorybookProcess = process.env.npm_lifecycle_event === 'storybook'
+
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [
     vue(),
-    vueDevTools(),
+    !isStorybookProcess && vueDevTools(),
     tailwindcss(),
   ],
   resolve: {
