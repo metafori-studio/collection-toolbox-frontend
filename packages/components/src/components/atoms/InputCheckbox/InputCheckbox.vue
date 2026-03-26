01d881c9840c612baf8f719674cc762212923f94
Alexander Mravcak <alex.mravcak@gmail.com>
2026-03-26 20:57:17 +0100

    feat(compoennts): tweak filter components

--- packages/components/src/components/atoms/InputCheckbox/InputCheckbox.vue
+++ packages/components/src/components/atoms/InputCheckbox/InputCheckbox.vue
@@ -1,32 +1,35 @@
 <template>
   <label
     class="
-      flex justify-between border px-2 py-2 cursor-pointer text-sm
-      hover:rounded-lg hover:bg-primary-100 hover:border-transparent
+      flex justify-between items-center border border-transparent border-b-neutral-200 px-4 py-2 cursor-pointer text-sm
+      hover:bg-primary-50 hover:border-transparent
     "
     :class="{
-      'border border-transparent border-b-neutral-200': !checked,
-      'rounded-lg border border-primary-600 text-primary-600': checked,
+      'border-transparent text-primary-600': checked,
     }"
     :for="id"
   >
-    <div class="flex items-center gap-1">
+    <div class="flex items-center gap-1 min-w-0">
       <input
         :id="id"
         v-model="model"
         type="checkbox"
         class="hidden"
       >
-      <BaseIcon
-        v-if="checked"
-        icon="checkSquare"
-        weight="fill"
-      />
-      <BaseIcon
-        v-else
-        icon="square"
-      />
-      {{ label }}
+      <div>
+        <BaseIcon
+          v-if="checked"
+          icon="checkSquare"
+          weight="fill"
+        />
+        <BaseIcon
+          v-else
+          icon="square"
+        />
+      </div>
+      <span class="truncate">
+        {{ label }}
+      </span>
     </div>
     <div
       v-if="count !== null"
