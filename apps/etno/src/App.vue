aad575decb423919f2dc0d88da26cefb9264c085
Alexander Mravcak <alex.mravcak@gmail.com>
2026-03-31 13:46:50 +0200

    feat(etno): i18n setup

--- apps/etno/src/App.vue
+++ apps/etno/src/App.vue
@@ -2,9 +2,25 @@
   <div>
     <AppHeader />
     <router-view />
+
+    <div
+      v-if="isDev"
+      class="fixed z-50 left-1 bottom-1 p-1 bg-white flex gap-1"
+    >
+      <button
+        v-for="lang in ['sk', 'en']"
+        :key="lang"
+        class="px-1 border"
+        @click="$i18n.locale = lang"
+      >
+        {{ lang }}
+      </button>
+    </div>
   </div>
 </template>
 
 <script setup lang="ts">
 import AppHeader from '@/components/AppHeader.vue';
+
+const isDev = import.meta.env.DEV;
 </script>
