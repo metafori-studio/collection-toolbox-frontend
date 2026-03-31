f555afc3df59cafa8123dd6ae0a3de97e5f4cdc6
Alexander Mravcak <alex.mravcak@gmail.com>
2026-03-31 13:46:50 +0200

    feat(etno): i18n setup

---
+++ apps/etno/src/i18n/index.ts
@@ -0,0 +1,16 @@
+import { createI18n } from 'vue-i18n';
+
+const i18n = createI18n({
+  legacy: false,
+  globalInjection: true,
+  locale: 'sk',
+  fallbackLocale: 'sk',
+  messages: {
+    sk: {
+    },
+    en: {
+    },
+  },
+});
+
+export default i18n;
--- apps/etno/src/main.ts
+++ apps/etno/src/main.ts
@@ -1,8 +1,10 @@
 import '@metafori/components/assets/main.css';
 import router from '@/router';
+import i18n from '@/i18n';
 import { createApp } from 'vue';
 import App from './App.vue';
 
 const app = createApp(App);
 app.use(router);
+app.use(i18n);
 app.mount('#app');
