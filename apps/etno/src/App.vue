981ec5d5141f271305f5d1e2b8885ec58f480f09
Alexander Mravcak <alex.mravcak@gmail.com>
2026-04-01 16:53:41 +0200

    feat(etno): proper language switcher

--- apps/etno/src/App.vue
+++ apps/etno/src/App.vue
@@ -3,27 +3,10 @@
     <AppHeader />
     <router-view class="flex-1" />
     <AppFooter />
-
-    <div
-      v-if="isDev"
-      class="fixed z-50 left-1 bottom-1 p-1 bg-white flex gap-1"
-    >
-      <button
-        v-for="lang in ['sk', 'en']"
-        :key="lang"
-        class="px-1 border"
-        @click="$i18n.locale = lang"
-      >
-        {{ lang }}
-      </button>
-    </div>
   </div>
 </template>
 
 <script setup lang="ts">
 import AppHeader from '@/components/AppHeader.vue';
 import AppFooter from '@/components/AppFooter.vue';
-
-
-const isDev = import.meta.env.DEV;
 </script>
