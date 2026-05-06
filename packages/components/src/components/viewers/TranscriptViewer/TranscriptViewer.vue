5c8365e18824cf04d2d0921af9b184d9a919505d
Igor Rjabinin <igor.rjabinin@gmail.com>
2026-05-06 17:04:28 +0200

    refactor(transcript-viewer): simplify props and class bindings

--- packages/components/src/components/viewers/TranscriptViewer/TranscriptViewer.vue
+++ packages/components/src/components/viewers/TranscriptViewer/TranscriptViewer.vue
@@ -1,17 +1,17 @@
 <template>
   <component
-    :is="props.sidePanel ? 'aside' : BaseViewer"
-    :class="[
-      'bg-white',
-      props.sidePanel && 'w-full shrink-0 border-l border-neutral-200 md:w-[360px]',
-    ]"
+    :is="sidePanel ? 'aside' : BaseViewer"
+    :class="{
+      'bg-white': true,
+      'w-full shrink-0 border-l border-neutral-200 md:w-[360px]': sidePanel,
+    }"
   >
     <div class="flex max-h-[calc(100vh-56px)] flex-col gap-3 overflow-auto p-4">
       <h2 class="text-heading-4">
         Transcript
       </h2>
       <p class="whitespace-pre-line text-sm text-neutral-600">
-        {{ props.transcript }}
+        {{ transcript }}
       </p>
     </div>
   </component>
@@ -20,11 +20,12 @@
 <script setup lang="ts">
 import BaseViewer from '../BaseViewer';
 
-const props = withDefaults(defineProps<{
+const {
+  transcript,
+  sidePanel = false,
+} = defineProps<{
   transcript: string
   sidePanel?: boolean
-}>(), {
-  sidePanel: false,
-});
+}>();
 </script>
 
