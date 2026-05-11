9b2d8f62de537bff69197a374579c03cd9d07ec3
Alexander Mravcak <alex.mravcak@gmail.com>
2026-05-11 15:57:51 +0200

    feat: shared analytics

--- apps/etno/src/main.ts
+++ apps/etno/src/main.ts
@@ -1,10 +1,16 @@
 import '@metafori/components/assets/main.css';
 import router from '@/router';
 import i18n from '@/i18n';
+import { setupGtm, setupMatomo } from '@metafori/shared';
 import { createApp } from 'vue';
 import App from './App.vue';
 
 const app = createApp(App);
 app.use(router);
+const gtm = setupGtm(router);
+if (gtm) {
+  app.use(gtm);
+}
+setupMatomo();
 app.use(i18n);
 app.mount('#app');
