7cadf1b9dae6b92914976612701f72e09d335660
Alexander Mravcak <alex.mravcak@gmail.com>
2026-05-13 08:20:49 +0200

    feat: common AppHeader, AppFooter

---
+++ apps/etno/src/components/EtnoFooter.vue
@@ -0,0 +1,31 @@
+<template>
+  <AppFooter
+    :title="$t('appName')"
+    :legal-text="$t('footer.legalText')"
+    :partners="partners"
+  />
+</template>
+
+<script setup lang="ts">
+import { AppFooter } from '@metafori/components';
+
+import uesaLogo from '@/assets/footer-logos/uesa.svg';
+import dariahLogo from '@/assets/footer-logos/dariah.svg';
+import oglLogo from '@/assets/footer-logos/ogl.png';
+
+const partners = [
+  {
+    src: uesaLogo,
+    name: 'UESA SAV',
+  },
+  {
+    src: dariahLogo,
+    name: 'DARIAH',
+  },
+  {
+    src: oglLogo,
+    name: 'Off Grid Lab',
+  },
+];
+
+</script>
--- apps/etno/src/components/EtnoHeader.vue
+++ apps/etno/src/components/EtnoHeader.vue
@@ -1,14 +1,18 @@
 <template>
-  <div class="sticky left-0 right-0 top-0 z-30 h-14 bg-white grid grid-cols-[1fr_150px_1fr] lg:grid-cols-[1fr_600px_1fr] items-center gap-3 px-4 py-1 border-b border-b-neutral-200">
-    <div class="flex items-center gap-3">
+  <AppHeader
+    grid-classes="grid-cols-[1fr_150px_1fr] lg:grid-cols-[1fr_600px_1fr]"
+  >
+    <template #left>
       <RouterLink to="/">
         {{ $t('header.explore') }}
       </RouterLink>
-    </div>
-    <div class="text-center font-bold">
+    </template>
+
+    <template #center>
       <EtnoSearch />
-    </div>
-    <div class="flex items-center justify-end gap-3">
+    </template>
+
+    <template #right>
       <BaseButton
         variant="secondary"
         size="small"
@@ -62,11 +66,12 @@
           </BaseButton>
         </div>
       </BaseDropdown>
-    </div>
-  </div>
+    </template>
+  </AppHeader>
 </template>
 
 <script setup lang="ts">
+import { AppHeader } from '@metafori/components';
 import {
   BaseButton,
   BaseIcon,
