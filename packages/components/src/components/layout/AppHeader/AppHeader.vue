88d24a59f3bd5b59d62eb29d55ab8a2a14fa9566
Alexander Mravcak <alex.mravcak@gmail.com>
2026-05-13 08:20:49 +0200

    feat: common AppHeader, AppFooter

---
+++ packages/components/src/components/layout/AppHeader/AppHeader.vue
@@ -0,0 +1,24 @@
+<template>
+  <header
+    class="sticky left-0 right-0 top-0 z-30 h-14 bg-white grid items-center gap-3 px-4 py-1 border-b border-b-neutral-200"
+    :class="[gridClasses]"
+  >
+    <div class="flex items-center gap-3">
+      <slot name="left" />
+    </div>
+    <div class="flex items-center justify-center gap-3">
+      <slot name="center" />
+    </div>
+    <div class="flex items-center justify-end gap-3">
+      <slot name="right" />
+    </div>
+  </header>
+</template>
+
+<script setup lang="ts">
+const {
+  gridClasses = 'grid-cols-[1fr_150px_1fr]',
+} = defineProps<{
+  gridClasses?: string
+}>();
+</script>
