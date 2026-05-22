b6a13d524bad9841c7adcbfaada990050b4d1df3
Alexander Mravcak <alex.mravcak@gmail.com>
2026-05-22 20:52:35 +0200

    feat(archeo): implement error 404 view

---
+++ apps/archeo/src/views/Error404View.vue
@@ -0,0 +1,12 @@
+<template>
+  <div class="flex items-center justify-center py-8">
+    <ErrorState
+      title="Chyba 404"
+      text="V prípade pretrvávajúcich ťažkostí kontaktujte prevádzkovateľa služieb."
+    />
+  </div>
+</template>
+
+<script setup lang="ts">
+import { ErrorState } from '@metafori/components';
+</script>
