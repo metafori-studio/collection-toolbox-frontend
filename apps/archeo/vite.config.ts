54cdd518ba845b1d1521d562b6d1c2aac1f5a5d4
Alexander Mravcak <alex.mravcak@gmail.com>
2026-06-19 15:39:10 +0200

    chore: unify .env files and vite proxying

--- apps/archeo/vite.config.ts
+++ apps/archeo/vite.config.ts
@@ -1,20 +1,33 @@
 import { fileURLToPath, URL } from 'node:url';
 
-import { defineConfig } from 'vite';
+import { defineConfig, loadEnv } from 'vite';
 import vue from '@vitejs/plugin-vue';
 import vueDevTools from 'vite-plugin-vue-devtools';
 import tailwindcss from '@tailwindcss/vite';
 
 // https://vite.dev/config/
-export default defineConfig({
-  plugins: [
-    vue(),
-    vueDevTools(),
-    tailwindcss(),
-  ],
-  resolve: {
-    alias: {
-      '@': fileURLToPath(new URL('./src', import.meta.url)),
+export default defineConfig(({ mode }) => {
+  const envDir = fileURLToPath(new URL('.', import.meta.url));
+  const env = loadEnv(mode, envDir);
+
+  return {
+    plugins: [
+      vue(),
+      vueDevTools(),
+      tailwindcss(),
+    ],
+    resolve: {
+      alias: {
+        '@': fileURLToPath(new URL('./src', import.meta.url)),
+      },
     },
-  },
+    server: {
+      proxy: {
+        '/api': {
+          target: env.VITE_API_REMOTE_URL,
+          changeOrigin: true,
+        },
+      },
+    },
+  };
 });
