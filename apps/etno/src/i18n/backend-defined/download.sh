71cb536115713ed0e579616164c808a721dedf3a
rastislav-chynoransky <rastislav.chynoransky@gmail.com>
2026-06-24 16:56:28 +0200

    chore(etno): update staging url

--- apps/etno/src/i18n/backend-defined/download.sh
+++ apps/etno/src/i18n/backend-defined/download.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env sh
 set -eu
 DIR="$(cd "$(dirname "$0")" && pwd)"
-URL="https://etno.metafori.com/api/etno/translations"
+URL="https://etno.staging.metafori.com/api/etno/translations"
 
 curl --fail --show-error --silent --location --retry 3 --connect-timeout 10 --max-time 15 \
   --header "Accept: application/json" \
