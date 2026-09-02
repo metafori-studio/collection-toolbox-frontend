8f1e7d80d712423396371a336436dd28ff29a847
Alexander Mravcak <alex.mravcak@gmail.com>
2026-09-02 08:26:16 +0200

    fix(components): missing vue-i18n type errors

---
+++ packages/components/src/i18n/index.ts
@@ -0,0 +1,14 @@
+import { createI18n } from 'vue-i18n';
+
+const i18n = createI18n({
+  legacy: false,
+  globalInjection: true,
+  locale: 'sk',
+  fallbackLocale: 'sk',
+  messages: {
+    sk: {},
+    en: {},
+  },
+});
+
+export default i18n;
